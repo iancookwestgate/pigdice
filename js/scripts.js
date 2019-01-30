b58c4115f59f6d52a2f655e89fc18469526f88a8
Ian Cook Westgate and Philip Eskins <philip@>
2019-01-30 11:10:56 -0800

    allow two players to play

--- js/scripts.js
+++ js/scripts.js
@@ -12,42 +12,53 @@ $(document).ready(function() {
   $("#dice-pigz").click(function(event) {
     var roll = diceRoll();
     $("#pig-results").append(roll + " ");
-    scoreArr.push(roll);
-
+    if (roll === 1) {
+      total = 0;
+      $("#dice-pigz").hide();
+      $("#dice-pigz-hold").hide();
+      $("#dice-pigz2").show();
+      $("#dice-pigz-hold2").show();
+    } else {
+      scoreArr.push(roll);
+    }
     function getTotal(total, num) {
       return total + num;
     }
     var total = scoreArr.reduce(getTotal);
 
-    if (roll === 1) {
-      total = 0;
-      $("#dice.pigz").hide("slow", function() {
-
-      });
-      $("#dice.pigz2").show("slow", function() {
-        
-      });
-    }
-
     $("#pig-score").text(total);
-  })
+  });
+  $("#dice-pigz-hold").click(function(event) {
+    $("#dice-pigz").hide();
+    $("#dice-pigz-hold").hide();
+    $("#dice-pigz2").show();
+    $("#dice-pigz-hold2").show();
+  });
 
   $("#dice-pigz2").click(function(event) {
     var roll = diceRoll();
     $("#pig-results2").append(roll + " ");
-    scoreArr2.push(roll);
-
+    if (roll === 1) {
+      total = 0;
+      $("#dice-pigz").show();
+      $("#dice-pigz-hold").show();
+      $("#dice-pigz2").hide();
+      $("#dice-pigz-hold2").hide();
+    } else {
+      scoreArr2.push(roll);
+    }
     function getTotal(total, num) {
       return total + num;
     }
     var total = scoreArr2.reduce(getTotal);
 
-    if (roll === 1) {
-      total = 0;
-      $("#dice.pigz").show();
-      $("#dice.pigz2").hide();
-    }
-
     $("#pig-score2").text(total);
   })
+  $("#dice-pigz-hold2").click(function(event) {
+    $("#dice-pigz").show();
+    $("#dice-pigz-hold").show();
+    $("#dice-pigz2").hide();
+    $("#dice-pigz-hold2").hide();
+  });
+
 })
