daa0aeb9a9c3f17aa8b1f2640e770d66f113c663
Ian Cook Westgate and Philip Eskins <ian@>
2019-01-30 14:48:41 -0800

    add one player logic

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,68 @@ Player.prototype.calculate = function(scoreArr) {
   this.score = total + this.score;
 }
 
+function aiPlayer(computer) {
+  var aiArr = [];
+    for (var i = 0; i <= 1; i++) {
+      var roll = diceRoll();
+      if (roll === 1) {
+        aiArr.push(0);
+        break;
+      } else {
+        aiArr.push(roll);
+      }
+    }
+  computer.calculate(aiArr);
+  console.log(aiArr);
+  console.log(computer.score);
+  return (computer.score);
+}
+
 function diceRoll () {
   var random = Math.floor(Math.random() * 6)
   return random+1;
 }
 
-
-
 $(document).ready(function() {
   var player1 = new Player(0);
   var player2 = new Player(0);
   var scoreArr = [];
+  var computer = new Player(0);
+  // SELECT ONE PLAYER
+  $("#activate1P").click(function(event) {
+    $("#onePlayer").show();
+    $("#startScreen").hide();
+  })
+
+  // SELECT TWO PLAYERS
+  $("#activate2P").click(function(event) {
+    $("#twoPlayer").show();
+    $("#startScreen").hide();
+  })
+
+  // ONE PLAYER UI LOGIC
+  $("#one-pigz").click(function(event) {
+    var roll = diceRoll();
+    $("#one-results").append(roll + " ");
+    if (roll === 1) {
+      total = 0;
+      scoreArr=[];
+      var computerScore = aiPlayer(computer);
+      $("#ai-results").text(computerScore);
+    } else {
+      scoreArr.push(roll);
+    }
+  });
+  $("#one-pigz-hold").click(function(event) {
+    player1.calculate(scoreArr)
+    $("#one-score").text(player1.score);
+    var computerScore = aiPlayer(computer);
+    $("#ai-results").text(computerScore);
+  });
+
+  // TWO PLAYER UI LOGIC
   $("#dice-pigz").click(function(event) {
     var roll = diceRoll();
-
     $("#pig-results").append(roll + " ");
     if (roll === 1) {
       total = 0;
@@ -34,7 +82,6 @@ $(document).ready(function() {
     } else {
       scoreArr.push(roll);
     }
-
   });
   $("#dice-pigz-hold").click(function(event) {
     player1.calculate(scoreArr)
@@ -43,6 +90,7 @@ $(document).ready(function() {
     $("#dice-pigz-hold").hide();
     $("#dice-pigz2").show();
     $("#dice-pigz-hold2").show();
+    aiPlayer(computer);
   });
 
   $("#dice-pigz2").click(function(event) {
@@ -58,8 +106,7 @@ $(document).ready(function() {
     } else {
       scoreArr.push(roll);
     }
-
-  })
+  });
   $("#dice-pigz-hold2").click(function(event) {
     player2.calculate(scoreArr);
     $("#pig-score2").text(player2.score);
@@ -68,5 +115,4 @@ $(document).ready(function() {
     $("#dice-pigz2").hide();
     $("#dice-pigz-hold2").hide();
   });
-
 })
